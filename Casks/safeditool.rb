45954d49f564b9909c49bdc88896e21ccecb1929
Dan Federman <dfed@me.com>
2024-06-10 15:10:09 -0700

    safeditool 0.6.3

--- Casks/safeditool.rb
+++ Casks/safeditool.rb
@@ -1,15 +1,15 @@
 cask "safeditool" do
-  version "0.6.2"
+  version "0.6.3"
 
   on_arm do
-    sha256 "44e3e1f015715b28918b50dbf440bccb11c6172e0b708c6db50d480cd79bae86"
+    sha256 "a3bb30d71d4d6b1b7547c126023de19e17dabae571035720013fa5879be707bd"
 
     url "https://github.com/dfed/SafeDI/releases/download/#{version}/SafeDITool-arm64"
 
     binary "SafeDITool-arm64", target: "SafeDITool"
   end
   on_intel do
-    sha256 "f607e129fa7b8a76c689e50cfec01ded9da8c0baa5d19105e3f9d2137503ca85"
+    sha256 "33356367f6183e0459850d31db473c48c72f79f0f1607396c625dcb4c0060f33"
 
     url "https://github.com/dfed/SafeDI/releases/download/#{version}/SafeDITool-x86_64"
 
